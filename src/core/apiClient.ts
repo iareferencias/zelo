99ddcdd283374696a6e370c0b6c2af5c35f3f6f9
IA Referencias <iareferencias@gmail.com>
2026-03-08 01:40:05 -0300

    Apply EmDia Factory infrastructure (schema: lab_zelo)

--- src/core/apiClient.ts
+++ src/core/apiClient.ts
@@ -1,10 +1,12 @@
 export const API_BASE_URL = import.meta.env.VITE_EMDIA_API_URL || ''
+export const API_SCHEMA = 'lab_zelo'
 
 export async function apiFetch(path: string, options: RequestInit = {}) {
   const response = await fetch(`${API_BASE_URL}${path}`, {
     ...options,
     headers: {
       'Content-Type': 'application/json',
+      'x-schema': API_SCHEMA,
       ...(options.headers || {})
     }
   })
